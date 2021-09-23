27f6943acb7107d977840fc2a9a9bf1523d9ccd0
Juana Nakfour <jnakfour@redhat.com>
2021-09-23 15:32:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import kfp
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
@@ -7,4 +8,13 @@ print("Flask Server")
 
 app = Flask(__name__)
 print("HTTP Server started")
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def launchPipeline():
+    client = kfp.Client(host='http://ml-pipeline-ui:80')
+    print(client.list_experiments())
+    return 'Hello World'
+  
 app.run(host='0.0.0.0', port=8080)
+
+
